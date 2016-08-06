2b6f189cdc7ec7058853c50b78c3a2ccd68d95ed
ivanzevakin <ixvakin@gmail.com>
2016-08-06 10:45:45 +0300

    removed sockets

--- src/server.js
+++ src/server.js
@@ -42,10 +42,6 @@ app.use('/ws', (req, res) => {
   proxy.web(req, res, {target: targetUrl + '/ws'});
 });
 
-server.on('upgrade', (req, socket, head) => {
-  proxy.ws(req, socket, head);
-});
-
 // added the error handling to avoid https://github.com/nodejitsu/node-http-proxy/issues/527
 proxy.on('error', (error, req, res) => {
   let json;
